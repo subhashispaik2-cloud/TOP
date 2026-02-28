3c67cca7de0f2a7b363dac374ac36aa4de2c0cae
subhashispaik2-cloud <subhashispaik2@gmail.com>
2026-02-28 10:38:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+#    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     RESULTS_COUNT = int(os.environ.get("RESULTS_COUNT", 5))
     BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "True")
     MOVIE_WEBSITE = os.environ.get("MOVIE_WEBSITE")
